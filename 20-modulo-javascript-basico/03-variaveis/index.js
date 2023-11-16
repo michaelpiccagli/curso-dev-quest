b3d713d43f7460ac300a8d4754c0bf0c2c850ed1
Michael Piccagli <michaelp_@hotmail.com>
2023-11-16 12:20:41 -0300

    Curso HTML CSS JS Avançado

---
+++ 20-modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,13 @@
+var nomePessoa = "Michael"
+
+const emailPessoa = "michaelp_@hotmail.com"
+
+nomePessoa = "Ricardo"
+
+console.log(nomePessoa)
+
+let telefonePessoa = "55111445545"
+
+telefonePessoa = "119999999"
+
+console.log(telefonePessoa)
---
+++ 20-modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,13 @@
+/*
+String
+Number
+Boolean
+Null
+Undefined
+*/
+let nome = 'michael'
+let numero = 0.6666666
+let usuarioVerificado = true
+let pessoa = null
+let telefone = undefined
+console.log(telefone)
