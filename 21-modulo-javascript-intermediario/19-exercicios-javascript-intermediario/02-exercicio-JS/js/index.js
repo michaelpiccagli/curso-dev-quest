9e5952a339f77f01a265f2cb5e717577e240322f
Michael Piccagli <michaelp_@hotmail.com>
2024-01-03 18:45:20 -0300

    modulo js intermediario

---
+++ 21-modulo-javascript-intermediario/19-exercicios-javascript-intermediario/02-exercicio-JS/js/index.js
@@ -0,0 +1,14 @@
+// **Descrição da tarefa:**  Você deve criar um array com os seguintes números: 65, 44, 12 e 4. Após a criação, deverá utilizar o laço de repetição forEach para percorrer os itens e realizar a multiplicação de cada um por 10. Pode apresentar o resultado de cada multiplicação usando o console.log().
+
+// <aside>
+// 💡 **Dica**: É essencial começar pensando no objetivo do que precisa ser feito e no passo a passo pra chegar lá.
+    
+// NÃO SAIA TENTANDO ESCREVER CÓDIGO NO JS! Antes disso você precisa saber O QUE precisa ser feito e só depois COMO fazer. 🧙‍♂️
+
+// </aside>
+
+const numeros = [65, 44, 12, 4];
+
+numeros.forEach((item) => {
+    console.log(item * 10)
+});
