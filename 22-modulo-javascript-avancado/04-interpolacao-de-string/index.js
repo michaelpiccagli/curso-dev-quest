160647d8f8ce238a296d3e560e054dce086f2ac0
Michael Piccagli <michaelp_@hotmail.com>
2025-03-31 14:09:52 -0300

    aulas-js-avancado-03-e-04

---
+++ 22-modulo-javascript-avancado/04-interpolacao-de-string/index.js
@@ -0,0 +1,12 @@
+// const imprimirIformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro nome: " + primeiroNome + " ultimo nome: " + ultimoNome + ' idade: ' + idade
+// }
+
+// console.log(imprimirIformacoesPessoas('Michael', 'piccagli', 35));
+ 
+const imprimirIformacoesPessoas = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome} ultimo nome: ${ultimoNome} idade: ${idade}`
+}
+
+console.log(imprimirIformacoesPessoas('Michael', 'piccagli', 35));
+ 
